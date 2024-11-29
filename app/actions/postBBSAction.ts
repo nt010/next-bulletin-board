6b4e68208d515a6b17007c257057256011a567fc
nt <tomnkmr0120@gmail.com>
2024-11-29 14:00:32 +0900

    add: server action

---
+++ app/actions/postBBSAction.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { z } from "zod";
+import { formSchema } from "../bbs-posts/create/page";
+import prisma from "../../lib/prismaClient";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+export const postBBS = async ({username, title, content}: z.infer<typeof formSchema>) => {
+    await prisma.post.create({
+        data: {
+            username,
+            title,
+            content,
+        }
+    });
+
+    revalidatePath("/");
+    redirect("/");
+}
--- app/bbs-posts/create/page.tsx
+++ app/bbs-posts/create/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,53 +10,49 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
-import { Textarea } from '@/components/ui/textarea';
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Textarea } from "@/components/ui/textarea";
 import { useForm } from "react-hook-form";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import { postBBS } from "@/app/actions/postBBSAction";
 
-
-const formSchema = z.object({
-    username: z.string().min(2, {message: "ユーザー名は2文字以上で入力してください"}),
-    title: z.string().min(2, {message: "タイトルは2文字以上で入力してください"}),
-    content: z.string().min(10, {message: "内容は10文字以上で入力してください"}).max(140, {message: "内容は140文字以内で入力してください"}),
-
-})
+export const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "ユーザー名は2文字以上で入力してください" }),
+  title: z
+    .string()
+    .min(2, { message: "タイトルは2文字以上で入力してください" }),
+  content: z
+    .string()
+    .min(10, { message: "内容は10文字以上で入力してください" })
+    .max(140, { message: "内容は140文字以内で入力してください" }),
+});
 
 const CreatePage = () => {
-
   const router = useRouter();
-    const form = useForm({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            username: "",
-            title: "",
-            content: "",
-        }
-    })
-async function onSubmit(value: z.infer<typeof formSchema>) {
-  const {username, title, content} = value;
-  try {
-    await fetch("http://localhost:3000/api/post", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({username, title , content}),
-    });
-    router.push("/");
-    router.refresh();
-  } catch (error) {
-    console.error(error)
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      title: "",
+      content: "",
+    },
+  });
+  async function onSubmit(value: z.infer<typeof formSchema>) {
+    const { username, title, content } = value;
+    postBBS({ username, title, content });
   }
-}
 
   return (
-<Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3 w-1/2 px-7">
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-3 w-1/2 px-7"
+      >
         <FormField
           control={form.control}
           name="username"
@@ -64,7 +60,11 @@ async function onSubmit(value: z.infer<typeof formSchema>) {
             <FormItem>
               <FormLabel>UserName</FormLabel>
               <FormControl>
-                <Input placeholder="username" className="resize-none" {...field} />
+                <Input
+                  placeholder="username"
+                  className="resize-none"
+                  {...field}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -105,7 +105,7 @@ async function onSubmit(value: z.infer<typeof formSchema>) {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default CreatePage
+export default CreatePage;
