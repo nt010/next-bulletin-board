2bda9762ea4f851545319183b6738a8f57b702f0
nt <tomnkmr0120@gmail.com>
2025-01-11 00:03:30 +0900

    fix create-form

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -20,7 +19,9 @@ import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { postBBS } from "@/app/actions/postBBSAction";
+import Header from "../components/layouts/header/Header"; // ヘッダーをインポート
 
+// バリデーションスキーマ
 export const formSchema = z.object({
   username: z
     .string()
@@ -88,103 +89,116 @@ const CreatePage = () => {
     }
   };
 
-
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-3 w-1/2 px-7"
-      >
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
+    <>
+      {/* ヘッダー */}
+      <Header onSearch={() => {}} />
+
+      {/* メインコンテンツ */}
+      <div className="flex justify-center mt-10">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-3 w-1/2 px-7 bg-white p-6 shadow-md rounded-lg"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>UserName</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="username"
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Created-Content" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Content</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="todo"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Todo</FormLabel>
+                  <FormControl>
+                    <Input placeholder="やってほしいことを入力してください" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
             <FormItem>
-              <FormLabel>UserName</FormLabel>
+              <FormLabel>Upload Image(only jpg・jpeg・png)</FormLabel>
               <FormControl>
                 <Input
-                  placeholder="username"
-                  className="resize-none"
-                  {...field}
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
                 />
               </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input placeholder="Created-Content" {...field} />
-              </FormControl>
               <FormDescription>
-                This is your public display name.
+                アップロードした画像のプレビューが下に表示されます。
               </FormDescription>
-              <FormMessage />
+              {imagePreview && (
+                <div className="mt-2">
+                  <Image
+                    src={imagePreview}
+                    alt="Preview"
+                    width={200}
+                    height={200}
+                    className="rounded-lg"
+                  />
+                </div>
+              )}
             </FormItem>
-          )}
-        />
 
-        <FormField
-          control={form.control}
-          name="content"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Content</FormLabel>
-              <FormControl>
-                <Textarea placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="todo"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Todo</FormLabel>
-              <FormControl>
-                <Input placeholder="やってほしいことを入力してください" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormItem>
-          <FormLabel>Upload Image(only jpg・jpeg・png)</FormLabel>
-          <FormControl>
-            <Input type="file" accept="image/*" onChange={handleImageChange} />
-          </FormControl>
-          <FormDescription>
-            アップロードした画像のプレビューが下に表示されます。
-          </FormDescription>
-          {imagePreview && (
-            <div className="mt-2">
-              <Image
-                src={imagePreview}
-                alt="Preview"
-                width={200}
-                height={200}
-                className="rounded-lg"
-              />
-            </div>
-          )}
-        </FormItem>
-
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+            <Button type="submit" className="w-full">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </>
   );
 };
 
